d159661f5104d74e761be3a9cf4825372c71c09d
Vladimir <36823172+Vladimir-carr@users.noreply.github.com>
2020-12-07 11:28:54 +0300

    Update Starter.java

--- ecm-consulting-console/src/main/java/com/ecm/consulting/application/filereader/Starter.java
+++ ecm-consulting-console/src/main/java/com/ecm/consulting/application/filereader/Starter.java
@@ -5,12 +5,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Starter {
-    private static final Path path = Paths.get("Enter your root path here");
+    private static final Path PATH = Paths.get("Enter your root path here");
 
     public static void main(String[] args) {
-        DocumentCounter documentCounter = new LocalCounter(path);
+        DocumentCounter documentCounter = new LocalCounter(PATH);
         try {
-            documentCounter.totalCountOfDocuments(path);
+            documentCounter.totalCountOfDocuments(PATH);
         } catch (IOException e) {
             e.printStackTrace();
         }
